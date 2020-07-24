7e4294e1efabf74a6d92102d0911d306a3dd4914
Kunal <kunal26das@gmail.com>
2020-07-24 21:01:00 +0530

    added playground app module

---
+++ playground/src/main/java/androidx/essentials/playground/PlaygroundActivity.kt
@@ -0,0 +1,12 @@
+package androidx.essentials.playground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlaygroundActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playground)
+    }
+}
