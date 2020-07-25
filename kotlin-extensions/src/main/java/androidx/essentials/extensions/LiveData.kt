bee6a42dda33340c685d9c8fc212f7e34f90cf7e
Kunal <kunal26das@gmail.com>
2020-07-25 12:46:27 +0530

    added LiveData switchMap for multiple arguments

--- kotlin-extensions/src/main/java/androidx/essentials/extensions/LiveData.kt
+++ kotlin-extensions/src/main/java/androidx/essentials/extensions/LiveData.kt
@@ -2,7 +2,11 @@ package androidx.essentials.extensions
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 object LiveData {
     fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
@@ -12,5 +16,37 @@ object LiveData {
                 removeObserver(this)
             }
         })
+    }
+
+    fun <Y> switchMap(
+        vararg sources: LiveData<*>,
+        switchMapFunction: (list: List<Any?>) -> LiveData<Y>
+    ): LiveData<Y?>? {
+        val result = MediatorLiveData<Y?>()
+        CoroutineScope(Dispatchers.Default).launch {
+            sources.forEach {
+                CoroutineScope(Dispatchers.Main).launch {
+                    result.addSource(it, object : Observer<Any?> {
+                        var mSource: LiveData<Y>? = null
+                        override fun onChanged(x: Any?) {
+                            val newLiveData = switchMapFunction(sources.map { it.value })
+                            if (mSource === newLiveData) {
+                                return
+                            }
+                            if (mSource != null) {
+                                result.removeSource(mSource!!)
+                            }
+                            mSource = newLiveData
+                            if (mSource != null) {
+                                result.addSource(
+                                    mSource!!
+                                ) { y -> result.value = y }
+                            }
+                        }
+                    })
+                }
+            }
+        }
+        return result
     }
 }
