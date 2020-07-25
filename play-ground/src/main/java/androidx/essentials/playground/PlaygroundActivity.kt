2c6bfa2b9d33db78cf36a9fb89c3474690d67034
Kunal <kunal26das@gmail.com>
2020-07-25 06:44:24 +0530

    implemented network callback

--- play-ground/src/main/java/androidx/essentials/playground/PlaygroundActivity.kt
+++ play-ground/src/main/java/androidx/essentials/playground/PlaygroundActivity.kt
@@ -1,12 +1,27 @@
 package androidx.essentials.playground
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.essentials.network.NetworkCallback
+import com.google.android.material.snackbar.Snackbar
 
 class PlaygroundActivity : AppCompatActivity() {
+
+    private lateinit var content: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playground)
+        content = findViewById(android.R.id.content)
+        networkCallback()
+    }
+
+    private fun networkCallback() {
+        val networkCallback = NetworkCallback(this).register({
+            Snackbar.make(content, "Online", Snackbar.LENGTH_INDEFINITE).show()
+        }, {
+            Snackbar.make(content, "Offline", Snackbar.LENGTH_INDEFINITE).show()
+        })
     }
 }
