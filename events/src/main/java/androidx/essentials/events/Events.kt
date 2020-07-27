3f020c2d80d847a8ab9450c202e977b0ac40dc87
Kunal <kunal26das@gmail.com>
2020-07-27 17:49:51 +0530

    added clear disposable function

--- events/src/main/java/androidx/essentials/events/Events.kt
+++ events/src/main/java/androidx/essentials/events/Events.kt
@@ -1,17 +1,24 @@
 package androidx.essentials.events
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.PublishSubject
 
 object Events {
 
+    private val compositeDisposable = CompositeDisposable()
     private val events = PublishSubject.create<Any>()
 
     fun <T> subscribe(eventClass: Class<T>, action: (data: T) -> Unit) {
-        events.ofType(eventClass)
-            .subscribeOn(Schedulers.io())
-            .subscribe({ action(it) }, {})
+        compositeDisposable.add(
+            events.ofType(eventClass)
+                .subscribeOn(Schedulers.io())
+                .subscribe({ action(it) }, {})
+        )
     }
 
     fun publish(event: Any) = events.onNext(event)
+
+    fun clear() = compositeDisposable.clear()
+
 }
