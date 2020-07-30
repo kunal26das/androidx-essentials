cb111837433c3f59e13a73fd03cb505ad1f0b1f5
Kunal <kunal26das@gmail.com>
2020-07-30 06:37:09 +0530

    optimised

--- kotlin-extensions/src/main/java/androidx/essentials/extensions/View.kt
+++ kotlin-extensions/src/main/java/androidx/essentials/extensions/View.kt
@@ -4,12 +4,12 @@ import android.view.View
 import android.view.ViewTreeObserver
 
 object View {
-    fun View.onGlobalLayoutListener(action: (View) -> Unit) {
+    fun View.addOnGlobalLayoutListener(action: (View) -> Unit) {
         viewTreeObserver.addOnGlobalLayoutListener(object :
             ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
                 viewTreeObserver.removeOnGlobalLayoutListener(this)
-                action.invoke(this@onGlobalLayoutListener)
+                action.invoke(this@addOnGlobalLayoutListener)
             }
         })
     }
