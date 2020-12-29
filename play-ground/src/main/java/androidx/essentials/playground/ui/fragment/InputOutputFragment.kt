d7f39346470efa83ef1b1c0425e62d188863f3f3
Kunal Das <kunal26das@gmail.com>
2020-12-29 08:25:54 +0530

    play ground update

--- play-ground/src/main/java/androidx/essentials/playground/ui/fragment/InputOutputFragment.kt
+++ play-ground/src/main/java/androidx/essentials/playground/ui/fragment/InputOutputFragment.kt
@@ -2,7 +2,6 @@ package androidx.essentials.playground.ui.fragment
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.essentials.core.lifecycle.Fragment
 import androidx.essentials.playground.R
 import androidx.essentials.playground.databinding.FragmentInputOutputBinding
@@ -16,18 +15,21 @@ class InputOutputFragment : Fragment<FragmentInputOutputBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.sharedViewModel = sharedViewModel
+        binding.textInput.setOnCutListener { toast("$it") }
+        binding.textInput.setOnCopyListener { toast("$it") }
+        binding.textInput.setOnPasteListener { toast("$it") }
         binding.apply {
             validate.setOnClickListener {
-                Toast.makeText(
-                    requireContext(), "${
+                toast(
+                    "${
                         true
                             and textInput.isValid
                             and autoComplete.isValid
                             and startDate.isValid
                             and endDate.isValid
                             and chips.isValid
-                    }", Toast.LENGTH_SHORT
-                ).show()
+                    }"
+                )
             }
         }
     }
