faef2ebd229978e516f7ade939636a08b2aed3af
Kunal Das <kunal26das@gmail.com>
2021-02-16 22:09:44 +0530

    code cleanup

--- io/src/main/java/androidx/essentials/io/EditText.kt
+++ io/src/main/java/androidx/essentials/io/EditText.kt
@@ -15,23 +15,21 @@ internal class EditText @JvmOverloads constructor(
     private var onCopyAction: ((Editable?) -> Unit)? = null
     private var onPasteAction: ((Editable?) -> Unit)? = null
 
-    override fun onTextContextMenuItem(id: Int): Boolean {
-        return when (id) {
-            android.R.id.cut -> {
-                onCutAction?.invoke(text)
-                super.onTextContextMenuItem(id)
-            }
-            android.R.id.copy -> {
-                onCopyAction?.invoke(text)
-                super.onTextContextMenuItem(id)
-            }
-            android.R.id.paste -> {
-                super.onTextContextMenuItem(id).apply {
-                    onPasteAction?.invoke(text)
-                }
-            }
-            else -> true
+    override fun onTextContextMenuItem(id: Int) = when (id) {
+        android.R.id.cut -> {
+            onCutAction?.invoke(text)
+            super.onTextContextMenuItem(id)
         }
+        android.R.id.copy -> {
+            onCopyAction?.invoke(text)
+            super.onTextContextMenuItem(id)
+        }
+        android.R.id.paste -> {
+            super.onTextContextMenuItem(id).apply {
+                onPasteAction?.invoke(text)
+            }
+        }
+        else -> true
     }
 
     internal fun setOnCutListener(action: (Editable?) -> Unit) {
