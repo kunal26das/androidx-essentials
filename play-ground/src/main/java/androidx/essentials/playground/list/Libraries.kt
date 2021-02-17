89d087ec273f4183b78f12eebf717fed4be1cc57
Kunal Das <kunal26das@gmail.com>
2021-02-18 02:31:58 +0530

    play ground update

--- play-ground/src/main/java/androidx/essentials/playground/list/Libraries.kt
+++ play-ground/src/main/java/androidx/essentials/playground/list/Libraries.kt
@@ -4,37 +4,20 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MenuItem
 import android.view.ViewGroup
-import androidx.core.view.updateMargins
 import androidx.databinding.BindingAdapter
-import androidx.essentials.core.events.Events
-import androidx.essentials.core.utils.Resources.dp
 import androidx.essentials.list.List
-import androidx.essentials.list.view.ListItemView
 import androidx.essentials.playground.R
-import androidx.essentials.playground.databinding.ItemLibraryBinding
-import androidx.essentials.playground.ui.PlayGroundActivity
 import androidx.essentials.playground.view.LibraryView
 
 class Libraries @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.recyclerViewStyle
-) : List<MenuItem, ItemLibraryBinding>(context, attrs, defStyleAttr) {
+) : List<MenuItem, LibraryView>(context, attrs, defStyleAttr) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = LibraryView(
-        context = context, attachToRoot = false
-    ).apply {
-        (layoutParams as MarginLayoutParams).updateMargins(8.dp, 8.dp, 8.dp, 8.dp)
-    }.viewHolder
-
-    override fun onBindViewHolder(
-        holder: ListItemView.ViewHolder<MenuItem, ItemLibraryBinding>,
-        position: Int, item: MenuItem
-    ) {
-        holder.listItemView.binding.root.setOnClickListener {
-            Events.publish(PlayGroundActivity.Destination.getById(item.itemId))
-        }
-    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ) = LibraryView(context).viewHolder
 
     companion object {
 
--- play-ground/src/main/java/androidx/essentials/playground/view/LibraryView.kt
+++ play-ground/src/main/java/androidx/essentials/playground/view/LibraryView.kt
@@ -2,45 +2,38 @@ package androidx.essentials.playground.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import android.view.MenuItem
+import androidx.core.view.setMargins
+import androidx.essentials.core.events.Events
 import androidx.essentials.core.utils.Resources.dp
+import androidx.essentials.extensions.Coroutines.default
 import androidx.essentials.list.view.ListItemView
 import androidx.essentials.playground.R
 import androidx.essentials.playground.databinding.ItemLibraryBinding
+import androidx.essentials.playground.ui.PlayGroundActivity
 
 class LibraryView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.materialCardViewStyle,
-    attachToRoot: Boolean = DEFAULT_ATTACH_TO_ROOT
-) : ListItemView<MenuItem, ItemLibraryBinding>(
-    context, attrs, defStyleAttr, attachToRoot
-) {
+    defStyleAttr: Int = R.attr.materialCardViewStyle
+) : ListItemView<MenuItem>(context, attrs, defStyleAttr) {
 
-    private var onItemClickListener: OnItemClickListener? = null
-
-    override val binding = ItemLibraryBinding.inflate(
-        LayoutInflater.from(context), this, attachToRoot
-    )
+    override val layout = R.layout.item_library
+    override val binding by dataBinding<ItemLibraryBinding>()
 
     init {
         radius = 8f.dp
         elevation = 4f.dp
+        layoutParams?.setMargins(8.dp)
     }
 
-    override fun bind(item: MenuItem) {
-        binding.apply {
-            menuItem = item
-            executePendingBindings()
-            root.setOnClickListener {
-                onItemClickListener?.onClick(item)
-            }
+    override fun bind(item: MenuItem) = binding.apply {
+        menuItem = item
+        executePendingBindings()
+        root.setOnClickListener {
+            default { Events.publish(PlayGroundActivity.Destination.getById(item.itemId)) }
         }
     }
 
-    interface OnItemClickListener {
-        fun onClick(item: MenuItem)
-    }
 
 }
