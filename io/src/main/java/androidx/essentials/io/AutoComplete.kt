7ee551a066e737d7933810712160584fd22be9e3
Kunal Das <kunal26das@gmail.com>
2021-03-08 08:44:09 +0530

    add string array support

--- io/src/main/java/androidx/essentials/io/AutoComplete.kt
+++ io/src/main/java/androidx/essentials/io/AutoComplete.kt
@@ -2,10 +2,24 @@ package androidx.essentials.io
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.core.content.res.getResourceIdOrThrow
 import androidx.essentials.io.generic.GenericAutoComplete
 
 class AutoComplete @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.textInputStyle
-) : GenericAutoComplete<String>(context, attrs, defStyleAttr)
+) : GenericAutoComplete<String>(context, attrs, defStyleAttr) {
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.AutoComplete, defStyleAttr, 0).apply {
+            try {
+                array =
+                    resources.getStringArray(getResourceIdOrThrow(R.styleable.AutoComplete_array))
+            } catch (e: IllegalArgumentException) {
+            }
+            recycle()
+        }
+    }
+
+}
--- io/src/main/java/androidx/essentials/io/Chips.kt
+++ io/src/main/java/androidx/essentials/io/Chips.kt
@@ -2,10 +2,24 @@ package androidx.essentials.io
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.core.content.res.getResourceIdOrThrow
 import androidx.essentials.io.generic.GenericChipGroup
 
 class Chips @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.chipGroupStyle
-) : GenericChipGroup<String>(context, attrs, defStyleAttr)
+) : GenericChipGroup<String>(context, attrs, defStyleAttr) {
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.Chips, defStyleAttr, 0).apply {
+            try {
+                chips =
+                    resources.getStringArray(getResourceIdOrThrow(R.styleable.Chips_chips)).toSet()
+            } catch (e: IllegalArgumentException) {
+            }
+            recycle()
+        }
+    }
+
+}
