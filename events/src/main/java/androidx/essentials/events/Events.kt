f78e204d13c9091c8e456148c6ab5f959d2bf84e
Kunal Das <kunal26das@gmail.com>
2021-03-17 15:57:32 +0530

    add unsubscribe function

--- events/src/main/java/androidx/essentials/events/Events.kt
+++ events/src/main/java/androidx/essentials/events/Events.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
-import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.kotlin.ofType
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.PublishSubject
@@ -12,34 +12,40 @@ import io.reactivex.rxjava3.subjects.PublishSubject
 object Events : LifecycleObserver {
 
     val events: PublishSubject<Any> = PublishSubject.create()
-    val compositeDisposable = mutableMapOf<LifecycleOwner, CompositeDisposable>()
+    val compositeDisposable = mutableMapOf<LifecycleOwner, LinkedHashMap<Class<*>, Disposable>>()
 
     inline fun <reified T : Any> LifecycleOwner.subscribe(
         noinline action: ((data: T) -> Unit)? = null
     ) {
         if (!compositeDisposable.containsKey(this)) {
-            compositeDisposable[this] = CompositeDisposable()
+            compositeDisposable[this] = LinkedHashMap()
             lifecycle.addObserver(this@Events)
         }
-        compositeDisposable[this]?.add(
-            events.ofType<T>().apply {
-                subscribeOn(Schedulers.io())
-            }.subscribe({
-                action?.invoke(it)
-            }, {
-                it.printStackTrace()
-            })
-        )
+        with(compositeDisposable[this]!!) {
+            if (!containsKey(T::class.java)) {
+                put(T::class.java, events.ofType<T>().apply {
+                    subscribeOn(Schedulers.io())
+                }.subscribe({
+                    action?.invoke(it)
+                }, {
+                    it.printStackTrace()
+                }))
+            }
+        }
     }
 
-    fun LifecycleOwner.unsubscribe() = compositeDisposable[this]?.clear()
+    inline fun <reified T : Any> LifecycleOwner.unsubscribe() {
+        compositeDisposable[this]?.get(T::class.java)?.dispose()
+    }
 
     fun Any.publish(event: Any) = events.onNext(event)
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy(lifecycleOwner: LifecycleOwner) {
         lifecycleOwner.lifecycle.removeObserver(this)
-        compositeDisposable[lifecycleOwner]?.clear()
+        compositeDisposable[lifecycleOwner]?.forEach {
+            it.value.dispose()
+        }
         compositeDisposable.remove(lifecycleOwner)
     }
 
