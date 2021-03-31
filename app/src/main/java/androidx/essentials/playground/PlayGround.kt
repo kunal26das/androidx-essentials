108d17c6e5acc748c9d3c39aa693e11501a383cd
Kunal Das <kunal26das@gmail.com>
2021-03-31 05:33:40 +0530

    play ground update

--- app/src/main/java/androidx/essentials/playground/PlayGround.kt
+++ app/src/main/java/androidx/essentials/playground/PlayGround.kt
@@ -1,12 +1,13 @@
 package androidx.essentials.playground
 
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.essentials.core.Application
-import androidx.essentials.network.NetworkCallback
+import androidx.essentials.network.Network
 import androidx.essentials.playground.ui.auto_complete.AutoCompleteViewModel
 import androidx.essentials.playground.ui.chips.ChipsViewModel
 import androidx.essentials.playground.ui.date.DateViewModel
 import androidx.essentials.playground.ui.home.HomeViewModel
-import androidx.essentials.playground.ui.network.NetworkViewModel
 import androidx.essentials.playground.ui.preferences.SharedPreferencesViewModel
 import androidx.essentials.playground.ui.text_input.TextInputViewModel
 import androidx.essentials.playground.ui.time.TimeViewModel
@@ -19,20 +20,22 @@ class PlayGround : Application() {
         viewModel { HomeViewModel() }
         viewModel { TimeViewModel() }
         viewModel { ChipsViewModel() }
-        viewModel { NetworkViewModel() }
         viewModel { TextInputViewModel() }
         viewModel { AutoCompleteViewModel() }
         viewModel { SharedPreferencesViewModel() }
     }
 
-    private val modules by lazy {
-        single { NetworkCallback.getInstance(this) }
-    }
-
     override fun onCreate() {
         super.onCreate()
-        setOf(viewModels, modules)
+        setOf(viewModels)
         Stetho.initializeWithDefaults(this)
+        Network.init(this, NetworkRequest.Builder().apply {
+            addTransportType(NetworkCapabilities.TRANSPORT_VPN)
+            addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
+            addTransportType(NetworkCapabilities.TRANSPORT_BLUETOOTH)
+        }.build())
     }
 
 }
--- app/src/main/java/androidx/essentials/playground/ui/network/NetworkFragment.kt
+++ app/src/main/java/androidx/essentials/playground/ui/network/NetworkFragment.kt
@@ -3,21 +3,18 @@ package androidx.essentials.playground.ui.network
 import android.os.Bundle
 import android.view.View
 import androidx.essentials.core.lifecycle.owner.Fragment
+import androidx.essentials.network.Network
 import androidx.essentials.playground.R
 import androidx.essentials.playground.databinding.FragmentNetworkBinding
 
 class NetworkFragment : Fragment() {
 
     override val layout = R.layout.fragment_network
-    override val viewModel by viewModel<NetworkViewModel>()
     override val binding by dataBinding<FragmentNetworkBinding>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = viewModel
-        binding.networkStateButton.setOnClickListener {
-            viewModel.refreshNetworkState()
-        }
+        binding.network = Network
     }
 
 }
