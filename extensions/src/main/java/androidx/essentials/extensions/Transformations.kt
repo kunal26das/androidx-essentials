650122c613dd46b52e4325b5d9e9f256178e2e17
Kunal Das <kunal26das@gmail.com>
2021-06-02 15:29:48 +0530

    remove unnecessary coroutines

--- extensions/src/main/java/androidx/essentials/extensions/Transformations.kt
+++ extensions/src/main/java/androidx/essentials/extensions/Transformations.kt
@@ -1,7 +1,5 @@
 package androidx.essentials.extensions
 
-import androidx.essentials.extensions.Coroutines.default
-import androidx.essentials.extensions.Coroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
@@ -10,32 +8,24 @@ object Transformations {
     fun <Y> switchMap(
         vararg sources: LiveData<*>,
         switchMapFunction: (list: List<Any?>) -> LiveData<Y>
-    ): LiveData<Y> {
-        val result = MediatorLiveData<Y>()
-        default {
-            sources.forEach {
-                main {
-                    result.addSource(it, object : Observer<Any?> {
-                        var mSource: LiveData<Y>? = null
-                        override fun onChanged(x: Any?) {
-                            val newLiveData = switchMapFunction(sources.map { it.value })
-                            if (mSource == newLiveData) {
-                                return
-                            }
-                            if (mSource != null) {
-                                result.removeSource(mSource!!)
-                            }
-                            mSource = newLiveData
-                            if (mSource != null) {
-                                result.addSource(
-                                    mSource!!
-                                ) { y -> result.value = y }
-                            }
+    ) = MediatorLiveData<Y>().apply {
+        sources.forEach {
+            addSource(it, object : Observer<Any?> {
+                var mSource: LiveData<Y>? = null
+                override fun onChanged(x: Any?) {
+                    val newLiveData = switchMapFunction(sources.map { it.value })
+                    if (mSource == newLiveData) return
+                    if (mSource != null) {
+                        removeSource(mSource!!)
+                    }
+                    mSource = newLiveData
+                    if (mSource != null) {
+                        addSource(mSource!!) {
+                            value = it
                         }
-                    })
+                    }
                 }
-            }
+            })
         }
-        return result
     }
 }
