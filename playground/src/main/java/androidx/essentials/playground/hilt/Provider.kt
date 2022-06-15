8c6024b57c8b62c129a3f0cc3c21f5614e8ef2a1
Kunal Das <kunal26das@gmail.com>
2022-06-15 14:56:52 +0530

    cleanup

--- playground/src/main/java/androidx/essentials/playground/hilt/Provider.kt
+++ playground/src/main/java/androidx/essentials/playground/hilt/Provider.kt
@@ -1,26 +1,9 @@
 package androidx.essentials.playground.hilt
 
-import android.content.Context
-import androidx.essentials.playground.network.Retrofit
-import androidx.essentials.playground.repository.Api
-import androidx.essentials.playground.repository.LibraryRepository
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object Provider {
-
-    @Provides
-    fun getApi() = Retrofit.create(Api::class)
-
-    @Provides
-    fun getLibraryRepository(
-        @ApplicationContext context: Context,
-        api: Api
-    ) = LibraryRepository(context, api)
-
-}
+object Provider
