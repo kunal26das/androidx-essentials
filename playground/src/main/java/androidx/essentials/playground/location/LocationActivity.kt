e6605945d6ee41934829c920142fe54ad0ffacd3
Kunal Das <kunal26das@gmail.com>
2022-06-23 13:13:39 +0530

    location compose

--- playground/src/main/java/androidx/essentials/playground/location/LocationActivity.kt
+++ playground/src/main/java/androidx/essentials/playground/location/LocationActivity.kt
@@ -6,27 +6,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.essentials.playground.R
-import androidx.essentials.playground.databinding.ActivityLocationBinding
-import androidx.essentials.view.Activity
+import androidx.essentials.view.ComposeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LocationActivity : Activity() {
+class LocationActivity : ComposeActivity() {
 
-    override val layout = R.layout.activity_location
     private val viewModel by viewModels<LocationViewModel>()
-    override val binding by dataBinding<ActivityLocationBinding>()
 
     private val locationRepositoryPermission = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) {
-        if (it.values.reduce { a, b -> a or b }) {
-            binding.viewModel = viewModel
-            viewModel.getLastLocation()
+        if (it.values.isNotEmpty()) {
+            if (it.values.reduce { a, b -> a or b }) {
+                viewModel.getLastLocation()
+            }
         }
     }
 
+    @Composable
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         locationRepositoryPermission.launch(
@@ -34,6 +44,48 @@ class LocationActivity : Activity() {
                 ACCESS_FINE_LOCATION,
                 ACCESS_COARSE_LOCATION
             )
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp)
+            ) {
+                LatitudeTextField()
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp)
+            ) {
+                LongitudeTextField()
+            }
+        }
+    }
+
+    @Composable
+    private fun LatitudeTextField() {
+        val latitude by viewModel.latitude.observeAsState()
+        TextField(
+            label = { Text(text = getString(R.string.latitude)) },
+            value = latitude?.toString() ?: "",
+            onValueChange = {},
+            readOnly = true,
+        )
+    }
+
+    @Composable
+    private fun LongitudeTextField() {
+        val longitude by viewModel.longitude.observeAsState()
+        TextField(
+            label = { Text(text = getString(R.string.longitude)) },
+            value = longitude?.toString() ?: "",
+            onValueChange = {},
+            readOnly = true,
         )
     }
 
